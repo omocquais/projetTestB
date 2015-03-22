ea5b958d25164d1828991de9e142d79ef11131a7
Olivier MOCQUAIS <olivier.mocquais@gmail.com>
2015-03-22 18:05:48 +0100

    setup projet B

---
+++ src/main/java/samples/App.java
@@ -0,0 +1,39 @@
+package samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    
+    private App(){
+      System.out.println( "Hello World ProjetB!" );
+      String test=null;
+      if (test.isEmpty()){}
+      if (test.isEmpty()){}
+
+      if (test.isEmpty()){}
+
+      if (test.isEmpty()){}
+
+      if (test.isEmpty()){}
+
+      if (test.isEmpty()){}
+      if (test.isEmpty()){}
+      if (test.isEmpty()){}
+      if (test.isEmpty()){}
+      if (test.isEmpty()){}
+
+    }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        /*
+        String test=null;
+        if (test.isEmpty())
+            System.out.println("pb  ");
+        */
+    }
+}
